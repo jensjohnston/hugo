56ee401f4c2aece1cae16338d9fc53319a53b331
Jens Johnston <johnstonjr27@gmail.com>
2020-09-07 21:39:50 +0200

    fix intro

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,18 +21,19 @@ sections.forEach(section => {
 //Navigation 
 const bodyTag = document.querySelector("body")
 const menuToggle = document.querySelector("a.menu-toggle")
-const menuToggleText = menuToggle.querySelector("span")
+
 
 menuToggle.addEventListener("click", function () {
     bodyTag.classList.toggle("nav-open")
 
     if (bodyTag.classList.contains("nav-open")) {
-        menuToggleText.innerHTML = "Close"
-
-        gsap.to(".burger-top", { rotation: 45 })
+        gsap.to(".burger-top", { rotation: 45, transformOrigin: "50% 50%", y: 8})
+        gsap.to(".burger-bottom", { rotation: -45, transformOrigin: "50% 50%", y: -8 })
+        gsap.to(".burger-middle", { width: 0 })
     } else {
-        menuToggleText.innerHTML = "Menu"
+        gsap.to(".burger-top", { rotation: 0, y: 0})
+        gsap.to(".burger-bottom", { rotation: 0, y: 0 })
+        gsap.to(".burger-middle", { width: 0 })
     }
-
 
 })
